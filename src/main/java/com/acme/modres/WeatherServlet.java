21f681a56f7e7fa1f0900aa391c57bfd1c02e783
paulbarry <paulbarr@ie.ibm.com>
2025-05-23 15:53:11 +0100

    liberty ready

--- src/main/java/com/acme/modres/WeatherServlet.java
+++ src/main/java/com/acme/modres/WeatherServlet.java
@@ -253,9 +253,6 @@ public class WeatherServlet extends HttpServlet {
 
     Hashtable ht = new Hashtable();
 
-    ht.put("java.naming.factory.initial", "com.ibm.websphere.naming.WsnInitialContextFactory");
-    ht.put("java.naming.provider.url", "corbaloc:iiop:localhost:2809");
-
     InitialContext ctx = null;
     try {
       ctx = new InitialContext(ht);
